faa756318ada8c198c1a00457d7b0501ec0b8f11
jason-liquidx <jason.kek@liquidx.studio>
2025-04-22 14:44:33 +0800

    Update bybit_real_time.py

--- bybit_real_time.py
+++ bybit_real_time.py
@@ -21,7 +21,8 @@ def home():
     return "✅ I'm alive on Render"
 
 def run_web():
-    app.run(host='0.0.0.0', port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
 
 def keep_alive():
     t = Thread(target=run_web)
