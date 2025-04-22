b0c5d469805c1ccfb2104462168ac7c095ea2d3a
jason-liquidx <jason.kek@liquidx.studio>
2025-04-22 14:49:12 +0800

    Update bybit_real_time.py

--- bybit_real_time.py
+++ bybit_real_time.py
@@ -120,7 +120,7 @@ def aggregate_and_alert():
 
 # Schedule report at 6AM and 6PM Malaysia Time
 schedule.every().day.at("06:00").do(aggregate_and_alert)
-schedule.every().day.at("14:30").do(aggregate_and_alert)
+schedule.every().day.at("15:15").do(aggregate_and_alert)
 
 
 # Start WebSocket
