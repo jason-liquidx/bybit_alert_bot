8e2e880caa8d32d37a9618579d68d16e4c4854af
jason-liquidx <jason.kek@liquidx.studio>
2025-05-23 12:38:31 +0800

    Update bybit_real_time.py

--- bybit_real_time.py
+++ bybit_real_time.py
@@ -160,5 +160,6 @@ def start():
     Thread(target=schedule_loop).start()
     Thread(target=schedule_heartbeat).start()
 
-if __name__ == "__main__":
-    start()
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))  # <-- required for Render
+    app.run(host='0.0.0.0', port=port)
